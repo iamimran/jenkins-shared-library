90922b3d4f0c800dae456771e6e3487e1292f42f
Imran Ahmed <iam.imran@yahoo.com>
2023-01-11 18:59:20 +0500

    updates BRANCH_ENV variable with env.BRANCH_NAME

--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
 def call() {
-    echo "Building the application for the $BRANCH_NAME"
+    echo "Building the application for the ${env.BRANCH_NAME}"
     sh "mvn package"
 }
