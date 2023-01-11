ae152f06747bf22693852f28f0b84b8f858f7078
Imran Ahmed <iam.imran@yahoo.com>
2023-01-11 18:47:46 +0500

    Adds paramter for image name

--- vars/buildAndPushDockerImage.groovy
+++ vars/buildAndPushDockerImage.groovy
@@ -1,9 +1,9 @@
 #!/usr/bin/env groovy
 
-def call() {
+def call(String imageName) {
     echo "Building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USERNAME')]) {
-        sh 'docker build -t iamimran/java-maven-app:jma1.0 .'
+        sh "docker build -t $imageName ."
         sh "echo $PASS | docker login -u $USERNAME --password-stdin"
         sh "docker push iamimran/java-maven-app:jma1.0"
     }
